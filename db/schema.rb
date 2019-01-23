03acad9a4b077718c34f50d182dd8d8bc0d004b4
sergioebp <sergioebp@gmail.com>
2019-01-23 15:36:24 -0300

    campo email

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_23_174235) do
+ActiveRecord::Schema.define(version: 2019_01_23_182516) do
 
   create_table "students", force: :cascade do |t|
     t.string "name"
     t.integer "age"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
   end
 
 end
